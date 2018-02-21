23cc143e3b7be759fd9a75dd303748a71aea8f63
Ajay Thorve <ajaythorve1994@gmail.com>
2018-02-21 11:46:08 -0500

    Update1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,78 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express, Ajay' });
 });
 
+const mysql = require('mysql');
+
+// First you need to create a connection to the db
+const collab = mysql.createConnection({
+    host: '54.156.197.224',
+    user: 'root',
+    password: 'edx',
+    database: 'collab_assess'
+});
+function collab_connect(collab){
+  console.log("here")
+  collab.connect((err) => {
+        if(err){
+            console.log("Error connecting to db"+err);
+            return;
+        }else{
+            console.log("connection established");
+        }
+    });
+}
+
+function collab_destroy(collab){
+    collab.end()
+}
+
+
+
+/* POST users room. */
+router.post('/getRoom', function(req, res, next) {
+    collab_connect(collab);
+    var curr_user = req.body.curr_user;
+    console.log(curr_user);
+    var room = 'room';
+    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
+    collab.query(query_statment,[curr_user,curr_user],(err,rows)=>{
+        console.log(rows);
+        if(err){
+            console.log(err);
+            throw err;
+        }else{
+            rows.forEach( (row) =>{
+                room = room+""+row.session_id+row.course_id
+            });
+        }
+
+    });
+    collab_destroy(collab);
+    res.send(room);
+});
+
+router.get('/getRoom', function(req, res, next) {
+    collab_connect(collab);
+    var curr_user = req.query.curr_user;
+    var room = 'room';
+    console.log(curr_user);
+    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
+    collab.query(query_statment,[curr_user,curr_user],(err,rows)=>{
+        console.log("rows: "+rows);
+        if(err){
+            console.log(err);
+            throw err;
+        }else{
+            console.log("rows: "+rows);
+            rows.forEach( (row) =>{
+
+                room = room+""+row.session_id+row.course_id
+            });
+        }
+
+    });
+    collab_destroy(collab);
+    res.send(room);
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql');
-
-// First you need to create a connection to the db
-const collab = mysql.createConnection({
-    host: '54.156.197.224',
-    user: 'edxapp001',
-    password: 'password',
-    database: 'collab_assess'
-});
-
-collab.connect((err) => {
-  if(err){
-    console.log("Error connecting to db");
-    return;
-  }else{
-      console.log("connection established");
-  }
-});
-
-
-
-/* GET users listing. */
-router.post('/getRoom', function(req, res, next) {
-  var curr_user = req.body.curr_user;
-  var room = 'room'
-  query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
-  collab.query(query_statment,[curr_user,curr_user],(err,rows)=>{
-      if(err){
-          console.log(err);
-          throw err;
-      }else{
-          rows.forEach( (row) =>{
-                room = room+""+row.session_id+row.course_id
-              });
-      }
-
-  });
-});
 
 
 
