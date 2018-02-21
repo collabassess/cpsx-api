af723dc67d0afdb637b6b582dbb391ca49538cfa
Ajay Thorve <ajaythorve1994@gmail.com>
2018-02-21 18:30:43 -0500

    Added connection pool mysql

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('../db_module/mysql');
 
+
+/* POST users room. */
+router.post('/getRoom', function(req, res, next) {
+    collab_connect(collab);
+    var curr_user = req.body.curr_user;
+    console.log(curr_user);
+    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
+    mysql.getConnection(function(err, conn){
+        conn.query(query_statment,[curr_user,curr_user],(err,rows)=>{
+            console.log(rows);
+            if(err){
+                console.log(err);
+                throw err;
+            }else{
+                rows.forEach( (row) =>{
+                    var room = "room"+row.session_id+row.course_id;
+                    collab_destroy(collab);
+                    mysql.release()
+                    res.send(room);
+                });
+            }
+
+        });
+    });
+});
+
+router.get('/getRoom', function(req, res, next) {
+    collab_connect(collab);
+    var curr_user = req.query.curr_user;
+    console.log(curr_user);
+    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
+    mysql.getConnection(function(err, conn){
+        conn.query(query_statment,[curr_user,curr_user],(err,rows)=>{
+            console.log(rows);
+            if(err){
+                console.log(err);
+                throw err;
+            }else{
+                rows.forEach( (row) =>{
+                    var room = "room"+row.session_id+row.course_id;
+                    collab_destroy(collab);
+                    mysql.release()
+                    res.send(room);
+                });
+            }
+
+        });
+    });
+
+});
 
 module.exports = router;
