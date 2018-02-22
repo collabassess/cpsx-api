233057abf3332e327379e6a869e5cdbabcda24c9
Ajay Thorve <ajaythorve1994@gmail.com>
2018-02-21 20:55:38 -0500

    Update2

--- db_module/cpsx_db.js
+++ db_module/cpsx_db.js
@@ -1,18 +1,11 @@
 const mysql = require('mysql');
 
 // First you need to create a connection to the db
-var pool  = mysql.createPool({
+var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'edx',
     database: 'collab_assess'
 });
 
-var getConnection = function(callback) {
-    pool.getConnection(function(err, connection) {
-        callback(err, connection);
-    });
-};
-
-exports.pool = getConnection;
-
+exports.pool = pool;
--- routes/users.js
+++ routes/users.js
@@ -8,46 +8,60 @@ var mysql = require('../db_module/cpsx_db').pool;
 router.post('/getRoom', function(req, res, next) {
     var curr_user = req.body.curr_user;
     console.log(curr_user);
-    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
-    mysql.getConnection(function(err, conn){
-        conn.query(query_statment,[curr_user,curr_user],(err,rows)=>{
-            console.log(rows);
+    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?';
+    mysql.getConnection(function (err, conn) {
             if(err){
-                console.log(err);
                 throw err;
-            }else{
-                rows.forEach( (row) =>{
-                    var room = "room"+row.session_id+row.course_id;
-                    collab_destroy(collab);
-                    mysql.release()
-                    res.send(room);
+            }
+            else{
+                conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
+                    console.log(rows);
+                    if (err) {
+                        console.log(err);
+                        throw err;
+                    } else {
+                        rows.forEach((row) => {
+                            var room = "room" + row.session_id + row.course_id;
+                            conn.release()
+                            res.send(room);
+                        });
+                    }
+
                 });
             }
 
         });
-    });
+
 });
 
 router.get('/getRoom', function(req, res, next) {
+    console.log("here1")
     var curr_user = req.query.curr_user;
     console.log(curr_user);
+    console.log("here2")
     query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
-    mysql.getConnection(function(err, conn){
-        conn.query(query_statment,[curr_user,curr_user],(err,rows)=>{
-            console.log(rows);
-            if(err){
-                console.log(err);
-                throw err;
-            }else{
-                rows.forEach( (row) =>{
-                    var room = "room"+row.session_id+row.course_id;
-                    collab_destroy(collab);
-                    mysql.release()
-                    res.send(room);
-                });
-            }
 
-        });
+    mysql.getConnection(function (err, conn) {
+        if(err){
+            console.log(err);
+        }
+        else{
+            conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
+                console.log(rows);
+                if (err) {
+                    console.log(err);
+                    throw err;
+                } else {
+                    rows.forEach((row) => {
+                        var room = "room" + row.session_id + row.course_id;
+                        conn.release()
+                        res.send(room);
+                    });
+                }
+
+            });
+        }
+
     });
 
 });
