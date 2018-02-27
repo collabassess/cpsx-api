86d7a2720e880b7cb71038255f94859abdf7f961
Ajay Thorve <ajaythorve1994@gmail.com>
2018-02-27 10:35:55 -0500

    test

--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,11 @@ function getUserRoom(curr_user){
         }
         else{
             conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
-                console.log(rows);
+                console.log(rows.length);
                 if (err) {
                     console.log(err);
                     throw err;
-                } else if(rows.length>0){
+                }else if(rows.length>0){
                     rows.forEach((row) => {
                         var room = "room" + row.session_id + row.course_id;
                         conn.release();
