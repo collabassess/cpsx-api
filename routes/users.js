f6d746cda4506fa480f18cefbf0da9525fffe226
Ajay Thorve <ajaythorve1994@gmail.com>
2018-02-27 10:22:19 -0500

    Changes

--- routes/users.js
+++ routes/users.js
@@ -4,46 +4,53 @@ var router = express.Router();
 var mysql = require('../db_module/cpsx_db').pool;
 
 
+function getUserRoom(curr_user){
+
+    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?';
+    mysql.getConnection(function (err, conn) {
+        if(err){
+            throw err;
+        }
+        else{
+            conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
+                console.log(rows);
+                if (err) {
+                    console.log(err);
+                    throw err;
+                } else {
+                    rows.forEach((row) => {
+                        var room = "room" + row.session_id + row.course_id;
+                        conn.release();
+                        return room;
+                    });
+                    conn.release();
+                    return "NaN";
+                }
+            });
+        }
+
+    });
+}
 /* POST users room. */
 router.post('/getRoom', function(req, res) {
     var curr_user = req.body.curr_user;
     console.log(curr_user);
-    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?';
-    mysql.getConnection(function (err, conn) {
-            if(err){
-                throw err;
-            }
-            else{
-                conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
-                    console.log(rows);
-                    if (err) {
-                        console.log(err);
-                        throw err;
-                    } else {
-                        rows.forEach((row) => {
-                            var room = "room" + row.session_id + row.course_id;
-                            conn.release();
-                            res.send(room);
-                        });
-                    }
-
-                });
-            }
-
-        });
+    var room = getUserRoom(curr_user);
+    if(room!=="NaN"){
+        res.send(room);
+    }else{
+        res.status(404)        // HTTP status 404: NotFound
+            .send('Not found');
+    }
 
 });
 
-router.get('/getRoom', function(req, res) {
-    console.log("here1")
-    var curr_user = req.query.curr_user;
-    console.log(curr_user);
-    console.log("here2")
-    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?'
-
+router.post("/initializeRoom",function (req,res) {
+    var curr_user = req.body.curr_user;
+    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?';
     mysql.getConnection(function (err, conn) {
         if(err){
-            console.log(err);
+            throw err;
         }
         else{
             conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
