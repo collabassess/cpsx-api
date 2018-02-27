120b88e847cbc738ec6cf33d6506ff0735284765
Ajay Thorve <ajaythorve1994@gmail.com>
2018-02-27 13:18:24 -0500

    intializeRoom

--- routes/users.js
+++ routes/users.js
@@ -4,75 +4,119 @@ var router = express.Router();
 var mysql = require('../db_module/cpsx_db').pool;
 
 
-function getUserRoom(curr_user){
-
+function getUserRoom(curr_user,callback){
+    var room = "";
     query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?';
     mysql.getConnection(function (err, conn) {
         if(err){
+            console.log("connection failed");
             throw err;
         }
-        else{
+        else {
             conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
                 if (err) {
-                    console.log(err);
+                    console.log("error: ", err);
                     throw err;
-                }else{
+                } else {
                     rows.forEach((row) => {
-                        var room = "room" + row.session_id + row.course_id;
+                        room = "room" + row.session_id + row.course_id;
                         conn.release();
-                        return room;
+                        callback(room);
                     });
+                    callback("NaN");
                 }
-                // else{
-                //     conn.release();
-                //     return "NaN";
-                // }
+            });
+        }
+    });
+}
+
+function upsertRoomUser(curr_user,callback) {
+    query_statment = 'SELECT * from user_groups\n' +
+        '                           WHERE user1 IS NULL OR user2 IS NULL';
+    console.log("here1");
+    mysql.getConnection(function (err, conn) {
+        if(err){
+            console.log("connection failed");
+            throw err;
+        }
+        else {
+            console.log("here2");
+            conn.query(query_statment, (err, rows) => {
+                console.log(rows);
+                if (err) {
+                    console.log("error:", err);
+                    throw err;
+                }else if(rows.length == 0){
+                    console.log("new row will be created");
+
+                    conn.query("INSERT INTO user_groups(course_id,user1)\n"+
+                                        " VALUES (?,?)", ['1',curr_user],(err) =>{
+                                    if(err) {
+                                        console.log("insert new row error");
+                                    }else{
+                                        conn.release();
+                                        callback("new room");s
+                                    }
+                        });
+                }else{
+                    console.log("old row updated");
+                    rows.forEach((row) => {
+                        session_id = row.session_id;
+                        course_id = row.course_id;
+                        user1 = row.user1;
+                        user2 = row.user2;
+                        if(user1 ==null){
+                            conn.query("UPDATE user_groups\n" +
+                                "SET user1=?\n" +
+                                "WHERE session_id=? && course_id=?",
+                                [curr_user, session_id, course_id],(err) => {
+                                    if (err) {
+                                        console.log("old row error");
+                                        throw err;
+                                    }
+                                });
+                        }else{
+                            conn.query("UPDATE user_groups\n" +
+                                "SET user2=?\n" +
+                                "WHERE session_id=? && course_id=?",
+                                [curr_user, session_id, course_id],(err) => {
+                                    if (err) {
+                                        console.log("old row error");
+                                        throw err;
+                                    }
+                                });
+                        }
+                        conn.release();
+                        temp_room = "room" + session_id + course_id;
+                        callback(temp_room);
+                    });
+
+                }
             });
         }
 
     });
+
 }
 /* POST users room. */
 router.post('/getRoom', function(req, res) {
     var curr_user = req.body.curr_user;
-    console.log(curr_user);
-    var room = getUserRoom(curr_user);
-    console.log(room);
-    if(room!=="NaN"){
-        res.send(room);
-    }else{
-        res.status(404)        // HTTP status 404: NotFound
-            .send('Not found');
-    }
-
+    getUserRoom(curr_user,function (room_value) {
+        res.send(room_value);
+    });
 });
 
 router.post("/initializeRoom",function (req,res) {
     var curr_user = req.body.curr_user;
-    query_statment = 'SELECT * from user_groups WHERE user1= ? OR user2= ?';
-    mysql.getConnection(function (err, conn) {
-        if(err){
-            throw err;
-        }
-        else{
-            conn.query(query_statment, [curr_user, curr_user], (err, rows) => {
-                console.log(rows);
-                if (err) {
-                    console.log(err);
-                    throw err;
-                } else {
-                    rows.forEach((row) => {
-                        var room = "room" + row.session_id + row.course_id;
-                        conn.release();
-                        res.send(room);
-                    });
-                }
-
+    getUserRoom(curr_user,function (room_value) {
+        var final_room = room_value;
+        if(room_value === "NaN"){
+            upsertRoomUser(curr_user,function (response) {
+                final_room = response;
             });
         }
-
+        res.send(final_room);
     });
-
 });
 
 module.exports = router;
