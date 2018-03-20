f3696c862faf526bf5e6513830cd232f32436d35
Ajay Thorve <ajaythorve1994@gmail.com>
2018-03-20 15:02:26 -0400

    up

--- routes/onlinePool.js
+++ routes/onlinePool.js
@@ -327,13 +327,13 @@ function pairUsers(user1,user2,course_id, callback) {
     getSession(user1,user2,course_id, function(valid){
         if(valid === false){
             console.log("inside pair Users, insert query");
-            var status = "valid";
-            var query_statement = 'INSERT INTO user_groups(course_id,user1,user2,status) values(?,?,?,"valid")';
+            var query_statement123 = 'INSERT INTO user_groups(course_id,user1,user2,status) values(?,?,?,"valid")';
             mysql.getConnection('CP_AS',function (err,conn) {
                 if(err){
                     console.log("connection failed");
                 }else{
-                    conn.query(query_statement,[course_id,user1,user2],(err,result) => {
+                    console.log(query_statement123);
+                    conn.query(query_statement123,[course_id,user1,user2],(err,result) => {
                         conn.release();
                         if (err) {
                             callback(false);
